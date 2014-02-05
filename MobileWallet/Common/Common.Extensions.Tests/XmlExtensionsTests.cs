6adb3b96f99a017b7a4440d994002289b294823e
Vitaly Kovganko <dimanava1@gmail.com>
2014-02-05 13:19:59 +0000

    Add XmlExtensions.cs into Common.Extensions and XmlExtensionsTests.cs into Common.Extensions.Tests

---
+++ MobileWallet/Common/Common.Extensions.Tests/XmlExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using NUnit.Framework;
+
+namespace Common.Extensions.Tests
+{
+    [TestFixture]
+    public class XmlExtensionsTests
+    {
+
+        [Test]
+        public void SaveToXml_LoadFromXmlTest()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Template.xml");
+            if (File.Exists(path))
+                File.Delete(path);
+
+            var testObj = new TestClass();
+            testObj.SaveToXml(path);
+            var testResultObj = path.LoadFromXml<TestClass>();
+
+            Assert.NotNull(testResultObj);
+            Assert.AreEqual(testObj.Str, testResultObj.Str);
+            Assert.AreEqual(testObj.Str2, testResultObj.Str2);
+        }
+    }
+
+    [Serializable]
+    [XmlRoot(ElementName = "passTemplate", Namespace = "http://www.mobilewallet.com")]
+    public class TestClass
+    {
+        [XmlElement(ElementName = "Str")]
+        public string Str { get; set; }
+         [XmlElement(ElementName = "Str2")]
+        public string Str2 { get; set; }
+    }
+}
---
+++ MobileWallet/Common/Common.Extensions/XmlExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Common.Extensions
+{
+    public static class XmlExtensions
+    {
+        public static void SaveToXml(this object obj, string filePath, string custNamespace = null)
+        {
+            var xws = new XmlWriterSettings() { Indent = true, Encoding = Encoding.UTF8 };
+            var serializer = new XmlSerializer(obj.GetType());
+            using (var xmlWriter = XmlWriter.Create(filePath, xws))
+            {
+                if (custNamespace != null)
+                {
+                    var ns = new XmlSerializerNamespaces();
+                    ns.Add("", custNamespace);
+                    serializer.Serialize(xmlWriter, obj, ns);
+                }
+                else
+                {
+                    serializer.Serialize(xmlWriter, obj);
+                }
+            }
+        }
+
+        public static TResult LoadFromXml<TResult>(this string filePath, string custNamespace = null)
+        {
+            var doc = new XmlDocument();
+            doc.Load(filePath);
+
+            using (var read = new StringReader(doc.OuterXml))
+            using (var reader = new XmlTextReader(read))
+            {
+                if (custNamespace != null)
+                {
+                    var serializer = new XmlSerializer(typeof (TResult), custNamespace);
+                    return (TResult)serializer.Deserialize(reader);
+                }
+                else
+                {
+                    var serializer = new XmlSerializer(typeof(TResult));
+                    return (TResult)serializer.Deserialize(reader);
+                }
+            }
+        }
+    }
+}
