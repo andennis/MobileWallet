43a636b09f7e93625cda8df87af318f3982083c4
Denis Andreev <dnandreev@gmail.com>
2014-02-18 23:14:56 +0000

    Pass.Distribution.Web - DownloadController (trial)

--- MobileWallet/Pass.Distribution.Web/App_Start/RouteConfig.cs
+++ MobileWallet/Pass.Distribution.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Pass.Distribution.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Download", action = "Download" }
             );
         }
     }
---
+++ MobileWallet/Pass.Distribution.Web/Controllers/DownloadController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pass.Distribution.Web.Controllers
+{
+    public class DownloadController : Controller
+    {
+        //
+        // GET: /PassDistribution/
+        public FileResult Download(string id)
+        {
+            //TODO temp code just for test
+            string path = HttpContext.Server.MapPath("~/App_Data/TextFile1.txt");
+            return File(path, "text/plain");
+        }
+
+        public FileResult DownloadPass(string passToken)
+        {
+            //TODO temp code just for test
+            string path = HttpContext.Server.MapPath("~/App_Data/Test1.pkpass");
+            return File(path, "application/vnd.apple.pkpass");
+        }
+	}
+}
