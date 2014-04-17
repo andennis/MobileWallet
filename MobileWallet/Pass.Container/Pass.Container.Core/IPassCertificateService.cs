1f1636ce85337b6a8733fa5b1d23a264945bc7ed
Denis Andreev <dnandreev@gmail.com>
2014-04-17 14:27:16 +0000

    Pass certificate service architecture (not finished)

---
+++ MobileWallet/Pass.Container/Pass.Container.Core/IPassCertificateService.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Pass.Container.Core
+{
+    public interface IPassCertificateService
+    {
+        X509Certificate2 GetCertificate(int certId);
+    }
+}
