56d7cb73bd04d32447850bccd5bb2463f899e6d5
Denis Andreev <dnandreev@gmail.com>
2014-08-10 22:37:49 +0000

    FormControlExtensions.TextBoxFormForExt

--- MobileWallet/Common/Common.Web/FormControlExtensions.cs
+++ MobileWallet/Common/Common.Web/FormControlExtensions.cs
@@ -12,6 +12,19 @@ namespace Common.Web
             return html.TextBoxFor(expression, htmlAttributes);
         }
 
+        public static MvcHtmlString TextBoxFormForExt<TModel, TProperty>(this HtmlHelper<TModel> html, Expression<Func<TModel, TProperty>> expression, object htmlAttributes = null, string label = null)
+        {
+            var builder1 = new TagBuilder("div");
+            builder1.AddCssClass("col-sm-9 col-md-10");
+            builder1.InnerHtml = html.TextBoxFor(expression, new {@class = "form-control"}).ToHtmlString();
+
+            var builder2 = new TagBuilder("div");
+            builder2.AddCssClass("form-group");
+            builder2.InnerHtml = html.LabelFor(expression, label, new { @class = "control-label col-sm-3 col-md-2" }).ToHtmlString() + builder1;
+
+            return new MvcHtmlString(builder2.ToString());
+        }
+
         public static MvcHtmlString LabelForExt<TModel, TValue>(this HtmlHelper<TModel> html, Expression<Func<TModel, TValue>> expression, string labelText, object htmlAttributes = null)
         {
             return html.LabelFor(expression, labelText, htmlAttributes);
@@ -26,6 +39,11 @@ namespace Common.Web
         public static MvcHtmlString PasswordForExt<TModel, TProperty>(this HtmlHelper<TModel> html, Expression<Func<TModel, TProperty>> expression, object htmlAttributes = null)
         {
             return html.PasswordFor(expression, htmlAttributes);
+        }
+
+        public static MvcHtmlString CheckBoxForEx<TModel>(this HtmlHelper<TModel> html, Expression<Func<TModel, bool>> expression, object htmlAttributes = null)
+        {
+            return html.CheckBoxFor(expression, htmlAttributes);
         }
     }
 }
