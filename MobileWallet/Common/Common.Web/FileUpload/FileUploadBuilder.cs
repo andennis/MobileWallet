a0606c32c6f2e4e5a6c6e6b80985094c195c0bb3
Vitaly Kovganko <dimanava1@gmail.com>
2014-10-03 22:34:23 +0000

    Pass.Manager.Web: FileUpload helper (not finished)

---
+++ MobileWallet/Common/Common.Web/FileUpload/FileUploadBuilder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Common.Web.FileUpload
+{
+    public class FileUploadBuilder : IHtmlString
+    {
+        private string _name;
+        private readonly FileUploadEventBuilder _fileUploadEventBuilder;
+        private readonly IDictionary<string, object> _events;
+
+        public FileUploadBuilder()
+        {
+            _events = new Dictionary<string, object>();
+            _fileUploadEventBuilder = new FileUploadEventBuilder(_events);
+        }
+
+        public FileUploadBuilder Name(string name)
+        {
+            _name = name;
+            return this;
+        }
+
+        public FileUploadBuilder Events(Action<FileUploadEventBuilder> clientEventsAction)
+        {
+            clientEventsAction(_fileUploadEventBuilder);
+            return this;
+        }
+
+        public string ToHtmlString()
+        {
+            return RenderFileUpload();
+        }
+
+        private string RenderFileUpload()
+        {
+            var sb = new StringBuilder();
+            var mainTag = new TagBuilder("div");
+            mainTag.GenerateId(_name);
+            sb.AppendLine(mainTag.ToString());
+            sb.AppendLine(GetInitializationScript());
+
+            return sb.ToString();
+        }
+
+        private string GetInitializationScript()
+        {
+           string test = @"<form method='post'action='/kendo-ui/upload/submit'>
+                <div class='demo-section k-header'>
+                    <input name='files' id='files' type='file' />
+                    <input type='submit' value='Submit' class='k-button' />
+                </div>
+            </form>";
+
+            var scriptTag = new TagBuilder("script");
+            scriptTag.InnerHtml = test + @"$(document).ready(function () {
+                $('#" + _name + @"').kendoUpload();});";
+
+            return scriptTag.ToString();
+        }
+    }
+}
---
+++ MobileWallet/Common/Common.Web/FileUpload/FileUploadExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Common.Web.FileUpload
+{
+    public static class FileUploadExtension
+    {
+        public static FileUploadBuilder FileUpload(this HtmlHelper html)
+        {
+            return new FileUploadBuilder();
+        }
+    }
+}
