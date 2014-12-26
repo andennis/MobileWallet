cc1f7b1fc028650ef6cde4782cfb8fb235218cf1
Denis Andreev <dnandreev@gmail.com>
2014-12-26 17:55:25 +0000

    nothing special

--- MobileWallet/Pass.Manager/Pass.Manager.Web/Scripts/PopupWindow.js
+++ MobileWallet/Pass.Manager/Pass.Manager.Web/Scripts/PopupWindow.js
@@ -1,5 +1,17 @@
 ﻿(function ($) {
 
+    var template = $.parseHTML('<div id="popupWindowContent"></div>');
+
+    function show($wnd) {
+        $.post();
+
+        $wnd.center().open();
+    }
+
+    function close($wnd) {
+        $wnd.close();
+    }
+
     function init() {
         $('body').on('click', '[data-popup-window]', null, function (e) {
             e.preventDefault();
@@ -7,8 +19,7 @@
             var wndId = $this.data("popup-window");
 
             var wnd = $("#" + wndId).data("kendoWindow");
-            wnd.open();
-            wnd.center();
+            show(wnd);
         });
     }
 
