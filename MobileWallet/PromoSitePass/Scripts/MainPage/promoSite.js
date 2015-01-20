aa9ca22271cdb43f9d997e820f6fdcd07fc5a5bc
Dmitry Kovganko <vkovganko@gmail.com>
2015-01-20 13:54:54 +0000

    Promo page design (not finished)

--- MobileWallet/PromoSitePass/Scripts/MainPage/promoSite.js
+++ MobileWallet/PromoSitePass/Scripts/MainPage/promoSite.js
@@ -105,3 +105,35 @@ $(function() {
         $('.' + tempClassName).css('fill', '#1abc9c');
     });
 });
+
+//Replace all SVG images with inline SVG
+
+jQuery(window).load(function() {
+    jQuery('img.imgIcon').each(function() {
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass);
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+            //$svg.css('fill', jQuery('#labelColorPicker').val());
+
+            jQuery('#' + imgID + ' path').css('fill', '#f25b3b');
+        }, 'xml');
+
+    });
+});
+
