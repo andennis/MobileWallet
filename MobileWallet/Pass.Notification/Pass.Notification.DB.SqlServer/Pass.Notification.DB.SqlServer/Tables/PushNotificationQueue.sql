c33cdc75fdfbf771484ff771a7c15532c3440b15
Vitaly Kovganko <dimanava1@gmail.com>
2015-01-28 14:31:44 +0000

    Pass.Notification: (in process)

---
+++ MobileWallet/Pass.Notification/Pass.Notification.DB.SqlServer/Pass.Notification.DB.SqlServer/Tables/PushNotificationQueue.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [pn].[PushNotificationQueue]
+(	
+    [PushNotificationId] INT IDENTITY (1, 1) NOT NULL,
+    [CertificateStorageId] INT NOT NULL,
+    [PushTockenId]  NVARCHAR(400) NOT NULL,
+    [CreatedDate]  DATETIME NOT NULL,
+    [UpdatedDate]  DATETIME NOT NULL,
+    [Status]  INT NOT NULL
+);
+
