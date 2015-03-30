4be51ae68df8d04deec106208f4fefdecb00a179
Denis Andreev <dnandreev@gmail.com>
2015-03-30 21:32:22 +0000

    Add missed files

---
+++ MobileWallet/FileStorage/FileStorage.Core/Entities/StorageFileInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FileStorage.Core.Entities
+{
+    public class StorageFileInfo : IDisposable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string OriginalName { get; set; }
+        public long Size { get; set; }
+        public Stream FileStream { get; set; }
+
+        public void Dispose()
+        {
+            if (FileStream != null)
+            {
+                FileStream.Dispose();
+                FileStream = null;
+            }
+        }
+    }
+}
