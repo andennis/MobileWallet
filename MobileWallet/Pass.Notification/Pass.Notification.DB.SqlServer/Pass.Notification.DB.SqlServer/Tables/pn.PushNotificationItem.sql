cc7e798f20b643c84811f23ff0c68cd14ecd8e19
Vitaly Kovganko <dimanava1@gmail.com>
2015-09-02 05:50:18 +0000

    Fix PushNotification sql schema

---
+++ MobileWallet/Pass.Notification/Pass.Notification.DB.SqlServer/Pass.Notification.DB.SqlServer/Tables/pn.PushNotificationItem.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [pn].[PushNotificationItem](
+	[PushNotificationItemId] [int] IDENTITY(1,1) NOT NULL,
+	[CertificateStorageId] [int] NOT NULL,
+	[PushTockenId] [nvarchar](max) NOT NULL,
+	[Status] [int] NOT NULL,
+	[PushNotificationServiceType] [int] NOT NULL,
+	[Version] [int] NOT NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[UpdatedDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_pn.PushNotificationItem] PRIMARY KEY CLUSTERED ([PushNotificationItemId] ASC)
+);
+
+
